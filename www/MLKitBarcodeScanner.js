b406d469124f25d90b16a242a0e57b1b0ed28191
NicolaasZA <lpretorius1995@gmail.com>
2022-08-26 09:22:04 +0200

    Update MLKitBarcodeScanner.js

--- www/MLKitBarcodeScanner.js
+++ www/MLKitBarcodeScanner.js
@@ -5,17 +5,17 @@ var PLUGIN_NAME = 'MLKitBarcodeScanner';
 var MLKitBarcodeScanner = {
 	/** Open the barcode scanning interface to scan one barcode, then return the result. */
 	scanBarcode: function (cameraFacing, success, failure) {
-		if (!cameraFacing || cameraFacing > 1 || cameraFacing < 0) { cameraFacing = 0; }
+		if (![0, 1].includes(cameraFacing)) { cameraFacing = 0; }
 
-		if (!success) { success = (barcode) => { }; }
-		if (!failure) { failure = (error) => { }; }
+		if (!success) { success = (_) => { }; }
+		if (!failure) { failure = (_) => { }; }
 
 		exec(success, failure, PLUGIN_NAME, "scanBarcode", [cameraFacing]);
 	},
 	/** Check if Google Play Services is available. Android ONLY. */
 	checkSupport: function (success, failure) {
-		if (!success) { success = (isSupported) => { }; }
-		if (!failure) { failure = (error) => { }; }
+		if (!success) { success = (_) => { }; }
+		if (!failure) { failure = (_) => { }; }
 
 		exec(success, failure, PLUGIN_NAME, "checkSupport", []);
 	}
